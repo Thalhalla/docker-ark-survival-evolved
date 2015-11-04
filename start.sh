944cf6e739c503ceb069577fd4ea06898bab73e4
James S. Moore <james@webtechhq.com>
2015-11-04 17:48:50 -0600

    fix steam params

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 
 cd /home/steam
 echo sed
-sed -i 's/steamuser="username"/steamuser=REPLACE_USER/' sdtdserver
-sed -i 's/steampass="password"/steampass=REPLACE_PASSWORD/' sdtdserver
-sed -i "s/steamuser=REPLACE_USER/steamuser='$STEAM_USERNAME'/" sdtdserver
-sed -i "s/steampass=REPLACE_PASSWORD/steampass='$STEAM_PASSWORD'/" sdtdserver
+sed -i 's/steamuser="username"/steamuser=REPLACE_USER/' arkserver
+sed -i 's/steampass="password"/steampass=REPLACE_PASSWORD/' arkserver
+sed -i "s/steamuser=REPLACE_USER/steamuser='$STEAM_USERNAME'/" arkserver
+sed -i "s/steampass=REPLACE_PASSWORD/steampass='$STEAM_PASSWORD'/" arkserver
 set_steam_guard_code $STEAM_GUARD_CODE
 yes y|./arkserver install
 cp -v ./serverconfig_template.xml ./serverfiles/ark-server.xml
