efb6e300582ed85fda8dc0cfd015ae6fce2ece56
James S. Moore <james@webtechhq.com>
2015-11-04 22:09:47 -0600

    update steampass/steamuser

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 cd /home/steam
 echo sed
-sed -i 's/steamuser="username"/steamuser=REPLACE_USER/' ShooterGameServer
-sed -i 's/steampass="password"/steampass=REPLACE_PASSWORD/' ShooterGameServer
+sed -i 's/steamuser="anonymous"/steamuser=REPLACE_USER/' ShooterGameServer
+sed -i 's/steampass=""/steampass=REPLACE_PASSWORD/' ShooterGameServer
 sed -i "s/steamuser=REPLACE_USER/steamuser='$STEAM_USERNAME'/" ShooterGameServer
 sed -i "s/steampass=REPLACE_PASSWORD/steampass='$STEAM_PASSWORD'/" ShooterGameServer
 set_steam_guard_code $STEAM_GUARD_CODE
